52172143eab4e355b64a56c747248c9aba503087
saumitra_Uchiha <35415639+Saumitra-Shukla@users.noreply.github.com>
2020-06-02 02:36:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ def find_version(*file_paths):
 setup(
     name='keras-bert',
     version=find_version('keras_bert', '__init__.py'),
-    packages=find_packages(),
     url='https://https://github.com/Saumitra-Shukla/keras-bert',
     license='MIT',
     author='Saumitra-Shukla',
